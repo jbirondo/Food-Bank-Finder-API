57a6395f7de87159648b46025dfbbeae3610f519
Eugene Birondo <jbirondo@gmail.com>
2020-12-04 00:56:13 -0800

    API basically finished

--- api/api.py
+++ api/api.py
@@ -8,10 +8,10 @@ from flask import request, jsonify, json
 from flask_cors import CORS
 import unicodedata
 
-# application = flask.Flask(__name__)
-# application.config["DEBUG"] = False
+application = flask.Flask(__name__)
+application.config["DEBUG"] = False
 
-# CORS(application, supports_credentials=True)
+CORS(application, supports_credentials=True)
 
 def get_food_banks(zip_code):
     headers = {
@@ -26,34 +26,33 @@ def get_food_banks(zip_code):
         'sec-fetch-dest': 'document',
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
-
     r = requests.get("https://www.freefood.org/zip.php?zip={}".format(zip_code), headers=headers)
     c = r.content
     soup = BeautifulSoup(c, "html.parser")
     all = soup.find_all("div", {"class": "event-box"})
     l = []
-
     for ele in all:
         b = {}
         b["Name"] = ele.find("h3").text
         b["Address"] = " ".join(ele.find("p").text.strip().replace("\n", "").replace("\r", "").split()).split("Phone:")[0]
-        
+        b["Phone"] = " ".join(ele.find("p").text.strip().replace("\n", "").replace("\r", "").split()).split("Phone:")[1].split("Fax:")[0].strip()
+        b["Image"] = ele.find("img")['src']
         l.append(b)
 
-    print(l)
+    return l
 
-# @application.route('/', methods=['GET'])
-# def home():
-#     return
+@application.route('/', methods=['GET'])
+def home():
+    return "<h1>Food Bank API</h1>"
 
 
-# @application.route('/<away>-<home>', methods=['GET'])
-# def matchup(away, home):
-#     return 
+@application.route('/<zip_code>', methods=['GET'])
+def matchup(zip_code):
+    return json.dumps({
+        "Data": get_food_banks(zip_code)
+    })
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))
-#     application.debug = True
-#     application.run(host='0.0.0.0', port=port)
-
-print(get_food_banks(35005))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    application.debug = True
+    application.run(host='0.0.0.0', port=port)
