01d1fadd924390456306612826780be3d37b74e1
Eugene Birondo <jbirondo@gmail.com>
2020-12-03 23:11:18 -0800

    Starting webscraper

--- api/api.py
+++ api/api.py
@@ -13,7 +13,10 @@ application.config["DEBUG"] = False
 
 CORS(application, supports_credentials=True)
 
-
+def get_food_banks(zip_code):
+    r = requests.get("https://shfb.auntbertha.com/food/food-pantry--ca?postal={}&filters=%7B%22attribute_tags%22%3A+%5B%22anyone+in+need%22%5D%7D&clearedfilter=1&cursor=0&limit=10".format(zip_code), headers={
+        'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+    
 
 
 @application.route('/', methods=['GET'])
