08213aefde1813f54f405bef2bdd2d91ff1ce559
Anthony Chan <appianchan@gmail.com>
2020-12-08 12:31:30 -0800

    did shelter component, renaming files

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Places from './places';
 import SearchBar from './SearchBar';
+import Shelter from './shelter';
 
 export default class Homepage extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            zipcode: 12345
+        }
+        this.zipcodeCallback = this.zipcodeCallback.bind(this);
+    }
+
+    zipcodeCallback(new_zipcode){
+        this.setState({zipcode: new_zipcode})
     }
 
     render(){
         return(
             <div>
-                <SearchBar />
-                <Places />
+                <SearchBar change_zip={this.zipcodeCallback}/>
+                <Places zipcode={this.state.zipcode}/>
+                <Shelter zipcode={this.state.zipcode}/>
             </div>
         )
     }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,14 +4,36 @@ export default class SearchBar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            zipcode: 94586
+            zipcode: "",
+            prev_zipcode: 94586
         }
+        this.changeZipCode = this.changeZipCode.bind(this);
+        this.changeArea = this.changeArea.bind(this);
+    }
+
+    changeZipCode(event) {
+        //handles change of input in state
+        console.log(event.target.value);
+        this.setState({ zipcode: event.target.value })
+        event.preventDefault();
+        // console.log(this.state.zipcode)
+    }
+    changeArea() {
+        this.props.change_zip(this.state.zipcode);
+        this.setState({ prev_zipcode: this.state.zipcode })
     }
 
     render(){
         return(
             <div>
-                This is where searchbar will be
+                <div>
+                    <div>Current zipCode = {this.state.prev_zipcode}</div>
+                    <div>
+                        <input value={this.state.zipcode} onChange={this.changeZipCode} type="text" name="zipcode" />
+                    </div>
+                    <button onClick={this.changeArea}>Change Zip Code</button>
+
+                </div>
             </div>
         )
     }
