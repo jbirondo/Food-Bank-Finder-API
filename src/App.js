6f0650707870e61c2e645e17dca0f42314c38858
Anthony Chan <appianchan@gmail.com>
2020-12-08 11:40:33 -0800

    added frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Places from './Components/places'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Title">
+        Food Bank Finder
+      </header>
+      <Places />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// export default class SearchBar extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             zipcode: 94586
+//         }
+//         this.changeZipCodeInState = this.changeZipCodeInState.bind(this);
+//         this.toAnalyze = this.toAnalyze.bind(this);
+//     }
+//     changeZipCodeInState(event){
+//         this.setState({zipcode: event.target.value})
+//         event.preventDefault();
+//     }
+
+//     toAnalyze(){
+//         this.props.callback(this.state.zipcode);
+//     }
+
+//     render(){
+//         return(
+            
+//         )
+//     }
+// }
