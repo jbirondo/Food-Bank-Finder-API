2704936dbbee58abf902332fc5fbcab2c1b13e8b
Anthony Chan <appianchan@gmail.com>
2020-12-09 14:49:17 -0800

    fix multiple marker issue, moving code to homepage file

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     this.state = {
       lng: -122.4193,
       lat: 37.7607,
-      zoom: 13,
+      zoom: 2,
     };
   }
 
@@ -23,25 +23,56 @@ class App extends React.Component {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom,
-      // interactive: false
+      interactive: false
     });
-
-    // https://docs.mapbox.com/help/tutorials/custom-markers-gl-js/
-
-    new mapboxgl.Marker()
-      .setLngLat([-122.4193, 37.7607])
-      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-      .setHTML("<h3>" + "hellow" + "</h3><p>" + "sup" + "</p>"))
-      .addTo(map);
     
-      new mapboxgl.Marker()
-        .setLngLat([-122, 37])
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML("<h3>" + "hellow" + "</h3><p>" + "sup" + "</p>")
-        )
-        .addTo(map);
-    
+      var geojson = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [-77.032, 38.913],
+            },
+            properties: {
+              title: "Mapbox",
+              description: "Washington, D.C.",
+            },
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [-122.414, 37.776],
+            },
+            properties: {
+              title: "Mapbox",
+              description: "San Francisco, California",
+            },
+          },
+        ],
+      };
+      geojson.features.forEach(function (marker) {
+        // create a HTML element for each feature
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker()
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.properties.title +
+                  "</h3><p>" +
+                  marker.properties.description +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+      });
+      
+        
   }
 
   render() {
@@ -65,15 +96,5 @@ class App extends React.Component {
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="Title">
-//         Food Bank Finder
-//       </header>
-//       <HomePage />
-//     </div>
-//   );
-// }
 
 export default App;
