69377c13fce9978de62a39c727e6602053f53348
Anthony Chan <appianchan@gmail.com>
2020-12-08 19:31:04 -0800

    zipcode logic done

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,8 +4,10 @@ export default class SearchBar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            zipcode: "",
-            prev_zipcode: 94586
+            zipcode: "0",
+            prev_zipcode: 94586,
+            error_message: false,
+            type: "",
         }
         this.changeZipCode = this.changeZipCode.bind(this);
         this.changeArea = this.changeArea.bind(this);
@@ -14,13 +16,40 @@ export default class SearchBar extends React.Component{
     changeZipCode(event) {
         //handles change of input in state
         console.log(event.target.value);
-        this.setState({ zipcode: event.target.value })
+        const x = typeof this.state.zipcode
+        this.setState({ zipcode: event.target.value , type: x})
         event.preventDefault();
         // console.log(this.state.zipcode)
     }
     changeArea() {
-        this.props.change_zip(this.state.zipcode);
-        this.setState({ prev_zipcode: this.state.zipcode })
+        // if ((typeof (this.state.zipcode) !== "number")){
+            
+        // }else{
+        //     this.props.change_zip(this.state.zipcode);
+        //     this.setState({ prev_zipcode: this.state.zipcode, error_message: false })
+        // }
+        var counter = true;
+        const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+        for (var y = 0; y < this.state.zipcode.length; y++) {
+            const letter = this.state.zipcode[y];
+            if (!numbers.includes(letter)) {
+                this.setState({ error_message: true })
+                counter = false;
+                break;
+            }
+        }
+        if(counter === true){
+            const trial = parseInt(this.state.zipcode, 10);
+            if(trial > 9999 && trial < 100000){
+                this.props.change_zip(this.state.zipcode);
+                this.setState({ prev_zipcode: this.state.zipcode, error_message: false })
+            }else{
+                this.setState({ error_message: true })
+            }
+            
+        }
+        
+        
     }
 
     render(){
@@ -28,9 +57,11 @@ export default class SearchBar extends React.Component{
             <div>
                 <div>
                     <div>Current zipCode = {this.state.prev_zipcode}</div>
+                    <div>State zip: {this.state.zipcode}, {this.state.type}</div>
                     <div>
                         <input value={this.state.zipcode} onChange={this.changeZipCode} type="text" name="zipcode" />
                     </div>
+                    <div style={{ visibility: this.state.error_message ? 'visible' : 'collapse' }}>Not a zipCode, please try again</div>
                     <button onClick={this.changeArea}>Change Zip Code</button>
 
                 </div>
